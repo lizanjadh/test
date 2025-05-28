275d46e85ae7a0206126115b682faebc870cf3ce
lizanjadh <36481693+lizanjadh@users.noreply.github.com>
2025-05-27 20:54:22 -0400

    Add files via upload

--- 900/lapse.mjs
+++ 900/lapse.mjs
@@ -1593,6 +1593,8 @@ async function patch_kernel(kbase, kmem, p_ucred, restore_info) {
 
     log('setuid(0)');
     sysi('setuid', 0);
+    log('kernel exploit succeeded!');
+    alert("kernel exploit succeeded!");
 }
 
 
