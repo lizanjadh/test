cca26eaccfb18947b05a534f29c28f0ec28aa412
lizanjadh <36481693+lizanjadh@users.noreply.github.com>
2025-06-07 00:00:43 -0400

    Add files via upload

--- 900/Jailbreak.js
+++ 900/Jailbreak.js
@@ -1,19 +1,23 @@
 export function GoldHEN() {
-    window.payload_path = './payloads/GoldHEN/GoldHEN.bin';
-    setTimeout(() => {
-        sessionStorage.setItem('jbsuccess', 1);
-        document.getElementById('jailbreak').style.display = 'flex';
-        document.getElementById('loader').style.display = 'none';
-        window.location.reload();
-    }, 3000); // 3 seconds delay
+    fetch('./payloads/GoldHEN/GoldHEN.bin').then(res => {
+        res.arrayBuffer().then(arr => {
+            window.pld = new Uint32Array(arr);
+            setTimeout(() => {
+            window.location.reload();
+            }, 3000); // 3 seconds delay
+            sessionStorage.setItem('jbsuccess', 1);
+        })
+    });   
 }
 
 export function HEN() {
-    window.payload_path = './payloads/HEN/HEN.bin';
-    setTimeout(() => {
-        sessionStorage.setItem('jbsuccess', 1);
-        document.getElementById('jailbreak').style.display = 'flex';
-        document.getElementById('loader').style.display = 'none';
-        window.location.reload();
-    }, 3000); // 3 seconds delay
+    fetch('./payloads/HEN/HEN.bin').then(res => {
+        res.arrayBuffer().then(arr => {
+            window.pld = new Uint32Array(arr);
+            setTimeout(() => {
+            window.location.reload();
+            }, 3000); // 3 seconds delay
+            sessionStorage.setItem('jbsuccess', 1);
+        })
+    });   
 }
